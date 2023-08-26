770874a63c84ef4a4a67052fcc922c7e9763641c
Omkar Nate <omkar.nate.dev@gmail.com>
2023-08-26 18:54:56 -0400

    Folder structure changes and added C# project

---
+++ Csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Oh come on. This is exactly how we all started.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
